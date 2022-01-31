61267f7c663fde9e9751fcf41fb1a9e61102d385
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2022-01-31 11:39:48 +0100

    Bump SDK version to 2.17.0-2.0.dev, which fixes conditional mirrors imports

--- lib/src/mirror_utils/mirror_utils.dart
+++ lib/src/mirror_utils/mirror_utils.dart
@@ -12,8 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-// Will not work yet; see https://github.com/dart-lang/sdk/issues/48057
-// Comment out `with_mirrors.dart` import manually to test
 export 'compiled.dart' if (dart.library.mirrors) 'with_mirrors.dart';
 export 'function_data.dart';
 export 'type_data.dart';
