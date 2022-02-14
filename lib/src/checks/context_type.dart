7b599d8473a486c0284c48a2bfbda70230f92ba6
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2022-02-14 15:00:41 +0100

    Document ChatCommandCheck

--- lib/src/checks/context_type.dart
+++ lib/src/checks/context_type.dart
@@ -16,7 +16,19 @@ class UserCommandCheck extends Check {
   UserCommandCheck() : super((context) => context is UserCommandCheck);
 }
 
+/// A check that succeeds if the command being invoked is a [ChatCommand].
+///
+/// This is generally used in combination with [Check.any] and/or [Check.deny] to only apply this
+/// check to [ChatCommand]s, while still registering this check to [CommandsPlugin].
+///
+/// You might also be interested in:
+/// - [InteractionChatCommandCheck], for checking that the command being executed is a [ChatCommand]
+///   and that it was invoked from an interaction;
+/// - [MessageChatCommandCheck], for checking that the command being executed is a [ChatCommand] and
+///   that it was invoked from a text message;
+/// - [InteractionCommandCheck], for checking that a command was invoked from an interaction.
 class ChatCommandCheck extends Check {
+  /// Create a new [ChatCommandCheck].
   ChatCommandCheck() : super((context) => context is IChatContext);
 }
 
