25dd0478dc8447801807fecf87069a7154bbc1ce
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2022-02-28 11:06:36 +0100

    Document boolConverter

--- lib/src/converters/converter.dart
+++ lib/src/converters/converter.dart
@@ -310,6 +310,16 @@ bool? convertBool(StringView view, IChatContext context) {
   return null;
 }
 
+/// A [Converter] that converts input to a [bool].
+///
+/// This converter will parse the input to `true` if the next word or quoted section of the input is
+/// one of `y`, `yes`, `+`, `1` or `true`. This comparison is case-insensitive.
+/// This converter will parse the input to `false` if the next work or quotetd section of the input
+/// is one of `n`, `no`, `-`, `0` or `false`. This comparison is case-insentive.
+///
+/// If the input is not one of the aforementioned words, this converter will fail.
+///
+/// This converter has a Discord Slash Command Argument Type of [CommandOptionType.boolean].
 const Converter<bool> boolConverter = Converter<bool>(
   convertBool,
   type: CommandOptionType.boolean,
